c244948e34d098ec615ba40624ea1392b72d2d12
semanticker <semanticker@gmail.com>
2021-10-20 23:15:13 +0900

    예제 앱 만들기-2   데이터바인딩, 로그인, 로그아웃

--- app/src/main/java/kr/asdfiop2021/mysampleapp/MainActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysampleapp/MainActivity.kt
@@ -5,15 +5,17 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kr.asdfiop2021.mysampleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
-   // private lateinit var binding: ActivityM
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,14 +25,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+
+        //
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         var btnJoinClicked = findViewById<Button>(R.id.btnJoin)
         btnJoinClicked.setOnClickListener {
 
             // 첫번째
-            var email = findViewById<EditText>(R.id.txtEmail)
-            var pwd = findViewById<EditText>(R.id.txtPwd)
+            // var email = findViewById<EditText>(R.id.txtEmail)
+            // var pwd = findViewById<EditText>(R.id.txtPwd)
 
             // 두번째
+            var email = binding.txtEmail;
+            var pwd = binding.txtPwd;
+
             auth.createUserWithEmailAndPassword(
                 email.text.toString(),
                 pwd.text.toString()
@@ -40,11 +50,36 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
 
                     } else {
-                        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+                        var msg = task.exception?.message;
+                        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
                     }
                 }
         }
 
+        binding.btnLogout.setOnClickListener{
+            auth.signOut()
+            Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnLogin.setOnClickListener{
+
+            var email = binding.txtEmail;
+            var pwd = binding.txtPwd;
+
+            auth.signInWithEmailAndPassword(
+                    email.text.toString(),
+                    pwd.text.toString()
+            )
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, auth.currentUser?.uid.toString(), Toast.LENGTH_SHORT).show()
+                        } else {
+                            var msg = task.exception?.message;
+                            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+        }
 
     }
 }
