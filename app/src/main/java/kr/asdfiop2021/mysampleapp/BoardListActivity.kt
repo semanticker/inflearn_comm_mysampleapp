c588242f41fae1255e745096159e883c271ee35f
semanticker <semanticker@gmail.com>
2021-10-22 23:38:48 +0900

    예제 앱 만들기 - 4   데이터베이스에서 데이터 받아오기

--- app/src/main/java/kr/asdfiop2021/mysampleapp/BoardListActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysampleapp/BoardListActivity.kt
@@ -3,7 +3,13 @@ package kr.asdfiop2021.mysampleapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class BoardListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +21,29 @@ class BoardListActivity : AppCompatActivity() {
             var intent = Intent(this, BoardWriteActivity::class.java)
             startActivity(intent)
         }
+
+        getData()
+    }
+
+    fun getData() {
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("board")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Log.d("myRef", dataSnapshot.toString())
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("BoardListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+
+        myRef.addValueEventListener(postListener)
     }
 }
